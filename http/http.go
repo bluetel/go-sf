b6a6e575910eee5d12fb485629d1b35c650f5818
David Avila Gracia <d.avilag23@gmail.com>
2019-06-29 11:37:59 +0100

    #2 - Using an object instead of a lot of parameters

--- http/http.go
+++ http/http.go
@@ -1,9 +1,9 @@
 package http
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"net/http"
 )
@@ -15,52 +15,55 @@ type Response struct {
 	Headers map[string][]string
 }
 
+//Request is the
+type Request struct {
+	URL     string
+	Headers map[string]string
+	Body    []byte
+}
+
 // Get executes a GET request against the specified URL.
-func Get(url string, headers map[string]string, out interface{}) (*Response, error) {
-	return request("GET", url, headers, out, nil)
+func Get(r Request, out interface{}) (*Response, error) {
+	return do("GET", r, out)
 }
 
 // Post executes a POST request against the specified URL.
-func Post(url string, headers map[string]string, out, payload interface{}) (*Response, error) {
-	return request("POST", url, headers, out, payload)
+func Post(r Request, out interface{}) (*Response, error) {
+	return do("POST", r, out)
 }
 
 // Patch executes a PATCH request against the specified URL.
-func Patch(url string, headers map[string]string, out, payload interface{}) (*Response, error) {
-	return request("PATCH", url, headers, out, payload)
+func Patch(r Request, out interface{}) (*Response, error) {
+	return do("PATCH", r, out)
 }
 
 // Put executes a PUT request against the specified URL.
-func Put(url string, headers map[string]string, out, payload interface{}) (*Response, error) {
-	return request("PUT", url, headers, out, payload)
+func Put(r Request, out interface{}) (*Response, error) {
+	return do("PUT", r, out)
 }
 
-func request(method, url string, headers map[string]string, out, payload interface{}) (*Response, error) {
-	var body io.Reader
-	if payload != nil {
-
-	}
+func do(method string, request Request, out interface{}) (*Response, error) {
 
 	// Build the request
-	req, err := http.NewRequest(method, url, body)
+	req, err := http.NewRequest(method, request.URL, bytes.NewBuffer(request.Body))
 	if err != nil {
-		return nil, fmt.Errorf("Error building the %s request for %s: %v", method, url, err)
+		return nil, fmt.Errorf("Error building the %s request for %s: %v", method, request.URL, err)
 	}
 
 	// Fill the request with the headers
-	for k, v := range headers {
+	for k, v := range request.Headers {
 		req.Header.Set(k, v)
 	}
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf("Error doing %s to %s: %v", method, url, err)
+		return nil, fmt.Errorf("Error doing %s to %s: %v", method, request.URL, err)
 	}
 	defer resp.Body.Close()
 
 	respBytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return nil, fmt.Errorf("Error reading the response when doiing %s to %s: %v", method, url, err)
+		return nil, fmt.Errorf("Error reading the response when doiing %s to %s: %v", method, request.URL, err)
 	}
 
 	// Initialising the response
@@ -68,7 +71,7 @@ func request(method, url string, headers map[string]string, out, payload interfa
 	if out != nil {
 		errJSON := json.Unmarshal(respBytes, out)
 		if errJSON != nil {
-			return nil, fmt.Errorf("Error decoding the response when doiing %s to %s: %v", method, url, err)
+			return nil, fmt.Errorf("Error decoding the response when doiing %s to %s: %v", method, request.URL, err)
 		}
 		response.Body = out
 	} else {
